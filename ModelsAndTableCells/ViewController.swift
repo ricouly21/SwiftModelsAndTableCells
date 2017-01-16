a0b548bc866006b632c6aa9d21bd2a00c8cd94ae
justine tabin <justinetabin@justines-MacBook-Pro.local>
2017-01-16 15:20:59 +0800

    Initial Commit

---
+++ ModelsAndTableCells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ModelsAndTableCells
+//
+//  Created by justine tabin on 16/01/2017.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

3a633ce922d8ed343608ec95cdf7089728e56e9c
ulyses rico <rico_uly21@yahoo.com>
2017-01-17 06:55:38 +0800

    Reworked and optimized view controllers

---
+++ ModelsAndTableCells/LabelTextViewTableCell.swift
@@ -0,0 +1,9 @@
+//
+//  LabelTextViewTableCell.swift
+//  ModelsAndTableCells
+//
+//  Created by justine tabin on 17/01/2017.
+//
+//
+
+import Foundation
---
+++ ModelsAndTableCells/ProfileEditViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileEditViewController.swift
+//  ModelsAndTableCells
+//
+//  Created by justine tabin on 17/01/2017.
+//
+//
+
+import Foundation
